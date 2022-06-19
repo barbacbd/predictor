4e18fc239378c4f73b246154f272052a1c42b065
Brent Barbachem <barbacbd@dukes.jmu.edu>
2022-06-19 01:11:27 +0000

    Updated Dockerfile and adding feast
    ** Added feast to dockerfile. Pulls and updates a specifc branch from
    FEAST. Builds the dependencies and installs python package.
    
    ** Added feature selection. This is only a start and other PRs/issues
    will take care of the rest by adding in the fixes.

--- predictor/features/feature_selection.py
+++ predictor/features/feature_selection.py
@@ -1,7 +1,12 @@
 from enum import Enum
+from ..log import get_logger
+
+
+log = get_logger()
+
 
 class FeatureSelectionType(Enum):
-
+    
     ALL = 0
     CMIM = 1
     discCMIM = 2
@@ -44,12 +49,106 @@ class FeatureSelectionType(Enum):
             return selection.name
 
 
-def select_features(files, selected_features):
-    '''Execute the provided FEAST algorithms (see FeatureSelectionType) in selected
-    features on all data provided in the files. These files should be the artifacts 
-    from executing the cluster analysis.
+try:
+    from feast.feast import (
+        CMIM,
+        discCMIM,
+        BetaGamma,
+        discBetaGamma,
+        CondMI,
+        discCondMI,
+        DISR,
+        discDISR,
+        ICAP,
+        discICAP,
+        JMI,
+        discJMI,
+        MIM,
+        discMIM,
+        mRMR_D,
+        disc_mRMR_D,
+        weightedCMIM,
+        discWeightedCMIM,
+        weightedCondMI,
+        discWeightedCondMI,
+        weightedDISR,
+        discWeightedDISR,
+        weightedJMI,
+        discWeightedJMI,
+        weightedMIM,
+        discWeightedMIM
+    )
     
-    :param files: Files output by the cluster analysis
-    :param selected_features: list of FEAST feature selection algorithms.
-    '''
+    TypeToFeastFunc = {
+        FeatureSelectionType.CMIM: CMIM,
+        FeatureSelectionType.discCMIM: discCMIM,
+        FeatureSelectionType.BetaGamma: BetaGamma,
+        FeatureSelectionType.discBetaGamma:discBetaGamma,
+        FeatureSelectionType.CondMI: CondMI,
+        FeatureSelectionType.discCondMI: discCondMI,
+        FeatureSelectionType.DISR: DISR,
+        FeatureSelectionType.discDISR: discDISR,
+        FeatureSelectionType.ICAP: ICAP,
+        FeatureSelectionType.discICAP: discICAP,
+        FeatureSelectionType.JMI: JMI,
+        FeatureSelectionType.discJMI: discJMI,
+        FeatureSelectionType.MIM: MIM,
+        FeatureSelectionType.discMIM: discMIM,
+        FeatureSelectionType.mRMR_D: mRMR_D,
+        FeatureSelectionType.disc_mRMR_D: disc_mRMR_D,
+        FeatureSelectionType.weightedCMIM: weightedCMIM,
+        FeatureSelectionType.discWeightedCMIM: discWeightedCMIM,
+        FeatureSelectionType.weightedCondMI: weightedCondMI,
+        FeatureSelectionType.discWeightedCondMI: discWeightedCondMI,
+        FeatureSelectionType.weightedDISR: weightedDISR,
+        FeatureSelectionType.discWeightedDISR: discWeightedDISR,
+        FeatureSelectionType.weightedJMI: weightedJMI,
+        FeatureSelectionType.discWeightedJMI: discWeightedJMI,
+        FeatureSelectionType.weightedMIM: weightedMIM,
+        FeatureSelectionType.discWeightedMIM: discWeightedMIM
+    }
+
+
+    def select_features(feature_selection_type, data, labels, num_features_to_select, weights=[]):
+        '''Run the feature selection algorithm provided as an enumeration in the `feature_selection_type`.
+        
+        :param feature_selection_type: value from `FeatureSelectionType`
+        :param data: main dataset 
+        :param labels: data set labels
+        :param num_features_to_select:
+        :param weights: Optional list of weights attached to all data values in the set
+        :return: 
+        '''
+        if feature_selection_type not in TypeToFeastFunc:
+            log.error(f"Failed to find feast function matching {feature_selection_type}")
+            return None
+        
+        if num_features_to_select <= 0:
+            log.error("Number of features to select must be greater than 0")
+            return None
+        
+        func = TypeToFeastFunc.get(feature_selection_type)
+        
+        if "weighted" in feature_selection_type.name.lower():
+            # check size of weights vs size of data
+            if not weights:
+                log.error("Weights parameter does not match size of data")
+                return None
+        
+            selected_features, feature_scores = func(data, labels, weights, num_features_to_select)
+        elif "BetaGamma" in feature_selection_type.name:
+            selected_features, feature_scores = func(data, labels, num_features_to_select, 1.0, 1.0)
+        else:
+            selected_features, feature_scores = func(data, labels, num_features_to_select)
+        
+        log.debug(selected_features, feature_scores)
+        
+        feature_names = data.columns.tolist()
+        return [feature_names[idx] for idx in selected_features]
     
+except ImportError as error:
+    log.error(error)
+    
+    def select_features(feature_selection_type, data, labels, num_features_to_select, weights=[]):
+        # empty function
+        return None
